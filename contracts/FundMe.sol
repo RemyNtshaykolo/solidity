fedf03b2a0226a44d66dd448beed651736f55bcc
"remy" <"remy.nts@gmail.com">
2021-12-12 11:49:34 +0100

    Fix sender

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -6,7 +6,6 @@ contract FundMe{
 
     AggregatorV3Interface priceFeed = AggregatorV3Interface("") 
     function convertWeiEther(uint256 amount) public view returns (uint256) {
-
         (,int256 data,,) = latestRoundData();
         return data;
     }
@@ -17,7 +16,7 @@ contract FundMe{
         transactions[msg.sender] = msg.value;
     }
 
-    function sayHello(address sender) public view returns( uint256 ) {
+    function showSender(address sender) public view returns( uint256 ) {
         return transactions[sender];
     }
 }
